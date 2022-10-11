b4e10bcb1010ca394212aade12a2017f5ac630eb
rasikaromesh <rasikaromesh000@gmail.com>
2022-10-11 11:50:09 +0800

    styled components in  input form

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,28 +1,65 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    margin-bottom: 0.5rem;
+    display: block;
+    font-weight: bold;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  .from-action {
+    margin: 1rem auto;
+  }
+`;
 
 const CourseInput = (props) => {
   const [inputGoal, setInputGoal] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const onChangeHandler = (event) => {
+    if (event.target.value.trim().length < 0) {
+      setIsValid(false);
+    }
     setInputGoal(event.target.value);
   };
 
   const onClickHandle = (event) => {
     event.preventDefault();
+    if (inputGoal.trim().length < 0) {
+      setIsValid(false);
+    }
     props.onAddGoal(inputGoal);
     setInputGoal("");
   };
+
   return (
     <form onSubmit={onClickHandle} className="form-control">
-      <div className="from-action">
+      <FormControl invalid={!isValid}>
         <label> Course Goal </label>
         <input type="text" value={inputGoal} onChange={onChangeHandler} />
-      </div>
-      <div className="form-actoin">
-        <Button type="submit" >Add Goal</Button>
-      </div>
+      </FormControl>
+      <FormControl>
+        <Button type="submit">Add Goal</Button>
+      </FormControl>
     </form>
   );
 };
